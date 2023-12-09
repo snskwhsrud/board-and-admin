129bb3fdf93197671e068a867f70d0c0179ec28b
snskwhsrud <wogur0309@naver.com>
2023-12-10 01:05:09 +0900

    #1 스프링부트 초기화
    #1 스프링을 뼈대를 생성

---
+++ board-and-admin/boardadmin/src/test/java/com/example/boardadmin/BoardadminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.boardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BoardadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
