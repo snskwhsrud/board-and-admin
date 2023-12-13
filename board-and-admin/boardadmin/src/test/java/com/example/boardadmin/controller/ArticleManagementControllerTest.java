25bf9d15b36e4eeb1b0a7c9ddb772ffb8fbc4bff
snskwhsrud <wogur0309@naver.com>
2023-12-13 23:08:47 +0900

    #10  뷰컨트롤러 수정

--- board-and-admin/boardadmin/src/test/java/com/example/boardadmin/controller/ArticleManagementControllerTest.java
+++ board-and-admin/boardadmin/src/test/java/com/example/boardadmin/controller/ArticleManagementControllerTest.java
@@ -9,32 +9,29 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글 관리")
-//@Import(SecurityConfig.class)
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleManagementController.class)
 class ArticleManagementControllerTest {
 
     private final MockMvc mvc;
+
     public ArticleManagementControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
     @DisplayName("[view][GET] 게시글 관리 페이지 - 정상 호출")
     @Test
-    void givenNothing_whenRequestingArticleManagementView_thenReturnsArticleManagementView() throws Exception{
-        //Given
+    void givenNothing_whenRequestingArticleManagementView_thenReturnsArticleManagementView() throws Exception {
+        // Given
 
-        //When & Then
+        // When & Then
         mvc.perform(get("/management/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("management/articles"));
     }
-
-
-
 }
