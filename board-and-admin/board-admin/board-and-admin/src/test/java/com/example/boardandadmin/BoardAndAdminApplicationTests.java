fac5824cfad327539ccece9a99813354788f0d0c
snskwhsrud <wogur0309@naver.com>
2023-12-16 00:39:59 +0900

    #14- 회원 관리 페이지
    이 pr은 회원 관리 페이지, 어드민 회원 페이지에 공통 레이아웃을 이용해 디자인을 입히고,
    데이터 테이블을 자바스크립트 플러그인을 사용해 그려낸다.
    JsGrid 테이블을 도입하고, 이를 위해 해당 플러그인을 추가한다. 아직 파일은 제대로 출력은 안됨 수정을 좀 해야함

---
+++ board-and-admin/board-admin/board-and-admin/src/test/java/com/example/boardandadmin/BoardAndAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.boardandadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BoardAndAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
